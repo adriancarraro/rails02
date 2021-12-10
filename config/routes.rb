15f788a0bba33b51a836cad726692f3bce1c2f2d
Adrian Carraro <a.carraro@donboscosandona.it>
2021-12-10 11:02:04 +0000

    modifiche

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/home/index'
   root 'home#index'
+
+  get '/home/index'
+  get '/home/chi_siamo'
 end
